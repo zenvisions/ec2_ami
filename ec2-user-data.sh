7ccfb52b1011bbdfe8fcf409658bb405f36b0c97
Steven Luckett <155846912+zenvisions@users.noreply.github.com>
2024-10-06 09:17:44 -0700

    Create ec2-user-data.sh

---
+++ ec2-user-data.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd 
+systemctl enable httpd
